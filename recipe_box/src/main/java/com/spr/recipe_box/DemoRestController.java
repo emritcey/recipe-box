f87d0fac5a6fc1dfc39fef7523e021ba127a89d4
Michelle Salgado <salgadom1129@gmail.com>
2020-03-06 09:05:24 -0600

    Added demo rest API in spring that talks to react

---
+++ recipe_box/src/main/java/com/spr/recipe_box/DemoRestController.java
@@ -0,0 +1,13 @@
+package com.spr.recipe_box;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+    @RequestMapping("/greeting")
+    public Message greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Message(name);
+    }
+}
