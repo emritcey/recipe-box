e8459bf313f2987feaf5430e9edd45704dba895a
emritcey <emma.ritcey@spr.com>
2020-03-10 11:16:05 -0500

    MP-30: Form Connection between DB & Node

--- node_app/Server.js
+++ node_app/Server.js
@@ -1,5 +1,7 @@
+const bodyParser = require("body-parser");
+
 // Import Middleware Functions
-const HelloWorldMiddleware = require("./Middleware/HelloWorld");
+const FetchOneUserMiddleware = require("./Middleware/FetchOneUser");
 
 // Instantiate express and App Object
 const express = require('express');
@@ -7,13 +9,15 @@ const app = express();
 
 // Declaring the Port variable
 const PORT = 8081;
+app.use(bodyParser.json());
 
 // Check ./Middleware/HelloWorld.js to see what is happening with "req.locals.phrase"
-app.get("/", HelloWorldMiddleware, (req, res) => {
-    // This Arrow Function is considered a Middleware Function
+app.get("/", FetchOneUserMiddleware, (req, res) => {
+    res.send(res.locals.data);
+});
 
-    // This method returns res.locals.phrase to the client with a 200 OK by default
-    res.send(res.locals.phrase);
+app.use("*", (req, res) => {
+    res.status(404).send("No Routes Matched");
 });
 
 // Invoke the express app to listen on the provided Port number
