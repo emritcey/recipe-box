5be066a119eb7f7a2231e9cca3964795d59d7222
emritcey <emma.ritcey@spr.com>
2020-03-24 11:22:09 -0500

    MP-54B: Adjust API Controller & add to readme

---
+++ recipe_box/src/main/java/com/spr/recipe_box/RecipeAPIController.java
@@ -0,0 +1,20 @@
+package com.spr.recipe_box;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/bigoven/recipes", produces = "application/json")
+public class RecipeAPIController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping("/{recipe_id}")
+    public List<Recipe> getRecipeById(@PathVariable String recipe_id) {
+        return recipeService.findById(recipe_id);
+    }
+}
