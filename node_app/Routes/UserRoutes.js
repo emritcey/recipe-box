9d3570269019dc970979f1136e29549e910781de
melissamclean <melmcl22@sbcglobal.net>
2020-04-17 00:53:02 -0500

    modularized server routes

---
+++ node_app/Routes/UserRoutes.js
@@ -0,0 +1,32 @@
+const FetchUserMiddleware = require("../Middleware/FetchUser");
+const AddUserMiddleware = require("../Middleware/Users/AddUser");
+
+const express = require('express');
+const router = express.Router();
+
+router.get("/", FetchUserMiddleware, (req, res) => {
+    if (res.locals.validUser) {
+        return res.send({nodeStatus: 200});
+    } else if (res.locals.nonValidUser) {
+        return res.send({ nodeStatus: 401 });
+    } else if (res.locals.error) {
+        return res.send({
+            nodeStatus: 404,
+            error: res.locals.error
+        });
+    }
+});
+
+router.post("/", AddUserMiddleware, (req, res) => {
+    if (res.locals.addedUser) {
+        return res.send({ nodeStatus: 200 });
+    } else if (res.locals.error) {
+        return res.send({
+            nodeStatus: 400,
+            error: res.locals.error
+        });
+    }
+});
+
+
+module.exports = router;
--- node_app/Server.js
+++ node_app/Server.js
@@ -1,14 +1,8 @@
 const bodyParser = require("body-parser");
 
 // Import Middleware Functions
-const FetchUserMiddleware = require("./Middleware/FetchUser");
-const AddUserMiddleware = require("./Middleware/Users/AddUser");
-
-const GetRecipeById = require("./Middleware/Recipes/GetRecipeById");
-const GetAllRecipes = require("./Middleware/Recipes/GetAllRecipes");
-const CreateRecipe = require("./Middleware/Recipes/CreateRecipe");
-const UpdateRecipe = require('./Middleware/Recipes/UpdateRecipe');
-const DeleteRecipe = require("./Middleware/Recipes/DeleteRecipe");
+const UserRoutes = require("./Routes/UserRoutes");
+const RecipeRoutes = require("./Routes/RecipeRoutes");
 
 // Instantiate express and App Object
 const express = require('express');
@@ -16,73 +10,11 @@ const app = express();
 
 // Declaring the Port variable
 const PORT = 80;
+
 app.use(bodyParser.json());
 
-// Check ./Middleware/HelloWorld.js to see what is happening with "req.locals.phrase"
-app.get("/user", FetchUserMiddleware, (req, res) => {
-    if (res.locals.validUser) {
-        return res.send({nodeStatus: 200});
-    } else if (res.locals.nonValidUser) {
-        return res.send({ nodeStatus: 401 });
-    } else if (res.locals.error) {
-        return res.send({
-            nodeStatus: 404,
-            error: res.locals.error
-        });
-    }
-});
-
-app.post("/user", AddUserMiddleware, (req, res) => {
-    if (res.locals.addedUser) {
-        return res.send({ nodeStatus: 200 });
-    } else if (res.locals.error) {
-        return res.send({
-            nodeStatus: 400,
-            error: res.locals.error
-        });
-    }
-});
-
-app.get("/recipes", GetAllRecipes, (req, res) => {
-    res.send(res.locals.data);
-});
-
-app.get("/recipes/:id", GetRecipeById, (req, res) => {
-    res.send(res.locals.data);
-});
-
-app.post("/recipes", CreateRecipe, (req, res) => {
-    if (res.locals.addedRecipe){
-        return res.send({ nodeStatus: 200 });
-    } else if(res.locals.error) {
-        return res.send({
-            nodeStatus:400,
-            error: res.locals.error
-        });
-    }
-});
-
-app.put("/recipes/:id", UpdateRecipe, (req, res) => {
-    if(res.locals.updatedRecipe){
-        return res.send({ nodeStatus: 200 });
-    } else if(res.locals.error) {
-        return res.send({
-            nodeStatus:400,
-            error: res.locals.error
-        });
-    }
-});
-
-app.delete("/recipes/:id", DeleteRecipe, (req, res) => {
-    if  (res.locals.deletedRecipe){
-        return res.send({ nodeStatus: 200 });
-    } else if(res.locals.error) {
-        return res.send({
-            nodeStatus: 400,
-            error: res.locals.error
-        });
-    }
-});
+app.use("/user", UserRoutes);
+app.use("/recipes", RecipeRoutes);
 
 app.use("*", (req, res) => {
     res.status(404).send("Node.js -> No Routes Matched");
